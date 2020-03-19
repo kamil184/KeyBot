540969d415b627b9aab7f7acb7be70d25775ec3b
AvanNorth <avannorthy@users.noreply.github.com>
2020-03-19 19:38:08 +0300

    try

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -7,8 +7,9 @@ public class Constants {
     public static final String STEAM = "Steam";
     public static final String ORIGIN = "Origin";
     public static final String UPLAY = "Uplay";
-    public static final String STEAMKEY30 = "Игра стим за 30 рублей";
-    public static final String STEAMKEY120 = "Игра стим за 120 рублей";
+    public static final String STEAMKEY30 = "Рандом игра стим за 15 рублей\nСтоймость игры 39-500 руб, ключ 100% рабочий!";
+    public static final String STEAMKEY120 = "Рандом игра стим за 50 рублей\nСтоймость игры 149-999 руб, ключ 100% рабочий!";
+    public static final String STEAMKEYAAA = "Рандом AAA игра стим за 250 рублей\nСтоймость игры 349-999 руб, ключ 100% рабочий!";
     public static final String MENU = "Меню";
     public static final String HELPTEXT = "Этот бот создан @SofHacker и @kamil184, по вопросам работоспособности бота и сотрудничества писать им в телегу.";
 }
