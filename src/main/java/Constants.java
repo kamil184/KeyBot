c7c87e24d886ad0fa63fafa8048ac6fe2e044ed6
AvanNorth <avannorthy@users.noreply.github.com>
2020-03-21 00:32:52 +0300

    s

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static final String STEAM = "Steam";
     public static final String ORIGIN = "Origin";
     public static final String UPLAY = "Uplay";
-    public static final String STEAMKEY15 = "Рандом игра стим за 15 рублей";
+    public static final String STEAMKEY15 = "Рандом игра стим за 10 рублей";
     public static final String INFOSTEAMKEY15 = "Рандом ключ от игры в стим\nСтоймость игры 39-499 рублей, ключ 100% рабочий!\nСписок игр:\n►3SwitcheD\n" +
             "►A Story About My Uncle\n" +
             "►Ablepsia\n" +
@@ -48,12 +48,12 @@ public class Constants {
             "►Two Worlds II Castle Defense\n" +
             "►Typical Nightmare\n" +
             "►WeakWood Throne";
-    public static final String STEAMKEY50 = "Рандом игра стим за 50 рублей";
-    public static final String HELPSTEAMKEY50 = "Рандом игра стим за 50 рублей?";
-    public static final String HELPSTEAMKEY15 = "Рандом игра стим за 15 рублей?";
-    public static final String HELPSTEAMKEYAAA = "Рандом ААА игра стим за 250 рублей?";
+    public static final String STEAMKEY50 = "Рандом игра стим за 25 рублей";
+    public static final String HELPSTEAMKEY50 = "Рандом игра стим за 25 рублей?";
+    public static final String HELPSTEAMKEY15 = "Рандом игра стим за 10 рублей?";
+    public static final String HELPSTEAMKEYAAA = "Рандом ААА игра стим за 199 рублей?";
     public static final String INFOSTEAMKEY50 = "Рандом ключ от игры в стим\nСтоймость игры 149-999 руб, ключ 100% рабочий!";
-    public static final String STEAMKEYAAA = "Рандом AAA игра стим за 250 рублей";
+    public static final String STEAMKEYAAA = "Рандом AAA игра стим за 199 рублей";
     public static final String INFOSTEAMKEYAAA = "Рандом ключ от AAA игры в стим. Стоймость игры 349-999 руб, ключ 100% рабочий!\nСписок игр:\n►Age of Wonders III |499p| [steam key];\n►Vendetta - Curse of Raven's Cry |515p| [steam key];\n►DiRT Rally |799p| [steam key]";
     public static final String MENU = "Меню";
     public static final String INFOTEXT = "Бот выдает рандомные ключи в определенном ценовом сегменте, но любая игра стоит ДОРОЖЕ, чем ключ, поэтому Вы можете испытывать удачу, не боясь уйти в минус! Мы стараемся улучшать бота и добавлять в него новые игры, разделы и платформы, спасибо , что покупаете у нас!\nВыберите раздел, по которому у Вас возник вопрос:";
